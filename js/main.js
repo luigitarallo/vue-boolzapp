a587f8ee73055c7096758eb7487ddf9fa6fe7a15
luigitarallo <tarallo.gigi@gmail.com>
2023-09-04 00:03:47 +0200

    feat: add comments

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ createApp({
   data() {
     return {
       contacts,
+      // Index
       activeChat: 0,
     };
   },
 
   methods: {
+    // Function to select chat
     clickChat(i) {
       this.activeChat = i;
     },
