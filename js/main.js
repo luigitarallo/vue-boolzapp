498f269bae13d91c66be1e5625c180ba0d69044d
luigitarallo <tarallo.gigi@gmail.com>
2023-09-04 16:19:00 +0200

    feat: add get date method to print time in new msg

--- js/main.js
+++ js/main.js
@@ -21,6 +21,21 @@ createApp({
     },
     addNewMessage() {
       const newMessageCopy = { ...this.newMessage };
+      const actualDate = new Date();
+      let day = actualDate.getDate();
+      let month = actualDate.getMonth() + 1;
+      let year = actualDate.getFullYear();
+      function addZero(i) {
+        if (i < 10) {
+          i = "0" + i;
+        }
+        return i;
+      }
+      let h = addZero(actualDate.getHours());
+      let m = addZero(actualDate.getMinutes());
+      let s = addZero(actualDate.getSeconds());
+      let time = h + ":" + m + ":" + s;
+      newMessageCopy.date = day + "/" + month + "/" + year + " " + time;
       this.contacts[this.activeChat].messages.push(newMessageCopy);
       this.newMessage = {};
     },
