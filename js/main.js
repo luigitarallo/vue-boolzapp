c8ab2dfe4ab2372645b22e803cbacd383ec85b0a
luigitarallo <tarallo.gigi@gmail.com>
2023-09-04 12:51:03 +0200

    feat: add addNewMessage function, newMsg empty obj

--- js/main.js
+++ js/main.js
@@ -6,6 +6,11 @@ createApp({
       contacts,
       // Index
       activeChat: 0,
+      newMessage: {
+        date: "",
+        message: "",
+        status: "sent",
+      },
     };
   },
 
@@ -13,6 +18,11 @@ createApp({
     // Function to select chat
     clickChat(i) {
       this.activeChat = i;
+    },
+    addNewMessage() {
+      const newMessageCopy = { ...this.newMessage };
+      this.contacts[this.activeChat].messages.push(newMessageCopy);
+      this.newMessage = {};
     },
   },
 }).mount("#app");
