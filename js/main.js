c7fbe80c4287af8cc2ca808b73f89450ff170a91
luigitarallo <tarallo.gigi@gmail.com>
2024-01-01 20:09:18 +0100

    feat: add methods to showm menu, delete message, print deleted message

--- js/main.js
+++ js/main.js
@@ -20,10 +20,50 @@ createApp({
         message: "OK!",
         status: "received",
       },
+      contacts: contacts.map((contact) => ({
+        ...contact,
+        messages: contact.messages.map((message) => ({
+          ...message,
+          showMenu: false, // Aggiungi questa proprietà per gestire la visibilità del menu
+        })),
+      })),
+      deletedMessageText:
+        '<span style="color: gray; font-style: italic;"><i class="fa-solid fa-circle-info"></i> Messaggio eliminato</span>',
     };
   },
 
+  mounted() {
+    // Aggiungi un ascoltatore di eventi di clic al livello superiore del DOM (body)
+    document.body.addEventListener("click", this.handleClickOutsideMenu);
+  },
+
+  beforeUnmount() {
+    // Rimuovi l'ascoltatore dell'evento di clic quando il componente viene distrutto
+    document.body.removeEventListener("click", this.handleClickOutsideMenu);
+  },
   methods: {
+    handleClickOutsideMenu(event) {
+      const menuElements = document.querySelectorAll(".message-options-menu");
+
+      menuElements.forEach((menu) => {
+        if (
+          !menu.contains(event.target) &&
+          !event.target.classList.contains("fa-chevron-down")
+        ) {
+          // Chiudi il menu solo se l'elemento cliccato non è parte del menu
+          this.contacts[this.activeChat].messages.forEach((message) => {
+            message.showMenu = false;
+          });
+        }
+      });
+    },
+
+    deleteMessage(index) {
+      // Segna il messaggio come cancellato impostando uno stato particolare
+      this.contacts[this.activeChat].messages[index].isDeleted = true;
+      this.contacts[this.activeChat].messages[index].showMenu = false; // Correzione qui
+    },
+
     // Function to select chat
     clickChat(i) {
       this.activeChat = i;
@@ -53,8 +93,19 @@ createApp({
       this.answearMessage();
     },
 
-    showMenu(index) {
-      console.log(index);
+    showMenu(index, event) {
+      event.stopPropagation();
+
+      // Chiudi tutti i menu tranne quello attualmente cliccato
+      this.contacts[this.activeChat].messages.forEach((message, i) => {
+        if (i !== index) {
+          message.showMenu = false;
+        }
+      });
+
+      // Inverti la visibilità del menu cliccato
+      this.contacts[this.activeChat].messages[index].showMenu =
+        !this.contacts[this.activeChat].messages[index].showMenu;
     },
 
     // Time Function to send Answear
